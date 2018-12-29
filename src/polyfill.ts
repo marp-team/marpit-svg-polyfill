c148e8dea1230af129d38a7d6054986cd49052d9
Yuki Hattori <yukihattori1116@gmail.com>
2018-12-29 16:41:30 +0900

    Support Marpit's advanced background

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -36,19 +36,20 @@ export function polyfill() {
       const { clientHeight, clientWidth } = svg
       if (!svg.style.transform) svg.style.transform = 'translateZ(0)'
 
-      const foreignObject = <SVGForeignObjectElement>svg.childNodes[0]
-      const foWidth = foreignObject.clientWidth
-      const foHeight = foreignObject.clientHeight
-      const scale = Math.min(clientHeight / foHeight, clientWidth / foWidth)
+      Array.from(svg.getElementsByTagName('foreignObject'), foreignObject => {
+        const foWidth = foreignObject.clientWidth
+        const foHeight = foreignObject.clientHeight
+        const scale = Math.min(clientHeight / foHeight, clientWidth / foWidth)
 
-      Array.from(foreignObject.getElementsByTagName('section'), section => {
-        if (!section.style.transformOrigin) {
-          section.style.transformOrigin = '0 0'
-        }
+        Array.from(foreignObject.getElementsByTagName('section'), section => {
+          if (!section.style.transformOrigin) {
+            section.style.transformOrigin = '0 0'
+          }
 
-        section.style.transform = `translate3d(${clientWidth / 2 -
-          (scale * foWidth) / 2}px,${clientHeight / 2 -
-          (scale * foHeight) / 2}px,0) scale(${scale})`
+          section.style.transform = `translate3d(${clientWidth / 2 -
+            (scale * foWidth) / 2}px,${clientHeight / 2 -
+            (scale * foHeight) / 2}px,0) scale(${scale})`
+        })
       })
     }
   })
